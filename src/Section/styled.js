9abbb4d5981705bebcbe4fd735a03cd01652bed3
Dorota Zielinska <dorcius@protonmail.com>
2023-06-06 19:39:44 +0200

    added styled component for component Section

---
+++ src/Section/styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+    background-color: white;
+    margin: 10px 0;
+`;
+
+export const Header = styled.header`
+    border-bottom: 1px solid #ccc;
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 20px;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+
+    @media (max-width: 767px){
+        grid-template-columns: 1fr; 
+    }
+`;
+
+export const Title = styled.h2`
+    font-size: 20px;
+    margin:0;
+`;
+
+export const SectionContainer = styled.div`
+    padding: 20px
+`;
