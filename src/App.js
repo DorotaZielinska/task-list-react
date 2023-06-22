14558090be24e603d36c1a2854c1f332e6c5728a
Dorota Zielinska <dorcius@protonmail.com>
2023-06-22 10:40:39 +0200

    added Hash Rooter, NavLink,Switch,Route,Redirect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom";
+import Tasks from "./features/tasks/TasksPage/Tasks";
+import TaskPage from "./features/tasks/TaskPage";
+import Author from "./features/tasks/author/Author";
 
-export default () => <Tasks />;
+export default () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink activeClassName="active" to="/zadania">
+                        Zadania
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink activeClassName="active" to="/autor">
+                        O autorze
+                    </NavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/zadania">
+                    <Tasks />
+                </Route>
+                <Route path="/autor">
+                    <Author />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </nav>
+
+    </HashRouter>
+)
