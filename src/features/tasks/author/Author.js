6a7d41b665e4b974ae61ab70f133da8e4cd0b271
Dorota Zielinska <dorcius@protonmail.com>
2023-06-22 10:36:44 +0200

    added Author

---
+++ src/features/tasks/author/Author.js
@@ -0,0 +1,15 @@
+import Container from "../../../common/Container";
+import Header from "../../../common/Header";
+import Section from "../../../common/Section";
+
+export default () => (
+    <Container>
+        <Header title="O autorze" />
+        <Section
+            title="Dorota Zielińska"
+            body={
+                <>nanan</>
+            }
+        />
+    </Container>
+);
