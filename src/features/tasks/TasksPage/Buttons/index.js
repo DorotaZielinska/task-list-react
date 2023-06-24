764caf2ea76df80ab867213e5b9d644ec4c258f1
Dorota Zielinska <dorcius@protonmail.com>
2023-06-24 11:37:13 +0200

    rename selector selectTasks to selectTasksState

--- src/features/tasks/TasksPage/Buttons/index.js
+++ src/features/tasks/TasksPage/Buttons/index.js
@@ -1,9 +1,9 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Container, StyledButtons } from "./styled";
-import { selectTasks, toggleHideDone, setAllDone, fetchExampleTasks } from "../../tasksSlice";
+import { toggleHideDone, setAllDone, fetchExampleTasks, selectTasksState } from "../../tasksSlice";
 
 const Buttons = () => {
-    const { tasks, hideDone } = useSelector(selectTasks);
+    const { tasks, hideDone } = useSelector(selectTasksState);
     const dispatch = useDispatch();
     return (
         <Container>
