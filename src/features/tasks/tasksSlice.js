30d9d63360e82ef7b5dbdf269da90761d38e267c
Dorota Zielinska <dorcius@protonmail.com>
2023-06-24 11:27:29 +0200

    added selectTasksByQuery and other selectors

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -36,10 +36,26 @@ const tasksSlice = createSlice({
     },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone, fetchExampleTasks, setTasks } = tasksSlice.actions;
-export const selectTasks = state => state.tasks;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone, fetchExampleTasks, setTasks} = tasksSlice.actions;
+export const selectTasksState = state => state.tasks;
+export const selectTasks = state => selectTasksState(state).tasks;
+export const selectHideDone = state => selectTasksState(state).hideDone;
+export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
+export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
 
 export const getTaskById = (state, taskId) =>
 selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTasksByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+
+    if (!query || query.trim() === ""){
+        return tasks;
+    }
+
+    return tasks.filter(({ content }) => content.toUpperCase().includes(query.trim().toUpperCase()));
+};
+
+
 
 export default tasksSlice.reducer;
